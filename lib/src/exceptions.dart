00400274cb1738122c543cdf77f490fe52ff6ec0
Hugo Pointcheval <hugo@gitlab.pointcheval.fr>
2020-04-15 17:42:24 +0200

    Add exceptions

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2020
+// Author: Hugo Pointcheval
+
+class KeyException implements Exception {
+  String error;
+  KeyException(this.error);
+}
+
+class EncryptionException implements Exception {
+  String error;
+  EncryptionException(this.error);
+}
+
+class DecryptionException implements Exception {
+  String error;
+  DecryptionException(this.error);
+}
+
+class NotImplementedException implements Exception {
+  String error;
+  NotImplementedException(this.error);
+}
