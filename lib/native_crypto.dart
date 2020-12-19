43de719ea6570dba0416f4180ab486ab22178fd6
Pointcheval Hugo <hugo@gitlab.pointcheval.fr>
2020-12-19 17:24:55 +0100

    Expose API

---
+++ lib/native_crypto.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2020
+// Author: Hugo Pointcheval
+export './src/exceptions.dart';
+export './src/key.dart';
+export './src/keyspec.dart';
+export './src/keyderivation.dart';
+export './src/digest.dart';
+export './src/cipher.dart';
+//export './src/kem.dart';
+export './src/platform.dart';
+export './src/utils.dart';
+
+export './src/sym/AES.dart';
+//export './src/asym/RSA.dart';
+
+const String version = "0.0.5";
+const String author = "Hugo Pointcheval";
+const String website = "https://hugo.pointcheval.fr/";
